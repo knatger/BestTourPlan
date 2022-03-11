c9564f59082b46aea03caf3b052952e76cc3bcbd
knatger <mandieva-natalya@mail.ru>
2022-03-11 14:05:33 +0500

    added parallax effect for newsletters section

--- js/main.js
+++ js/main.js
@@ -19,3 +19,5 @@ const reviewsSlider = new Swiper('.reviews-slider', {
     prevEl: '.reviews-slider__button--prev',
   },
 });
+
+$('.parallax-window').parallax({imageSrc: 'img/newsletter-bg.jpg'});
